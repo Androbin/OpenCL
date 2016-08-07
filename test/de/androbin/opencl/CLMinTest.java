a2d2951557e41756513769de9bc58ade6f08d99d
Androbin <robin.richtsfeld@gmail.com>
2016-08-07 16:38:22 +0200

    Add files via upload

---
+++ test/de/androbin/opencl/CLMinTest.java
@@ -0,0 +1,19 @@
+package de.androbin.opencl;
+
+import java.nio.*;
+import java.util.function.*;
+
+public final class CLMinTest implements CLTest
+{
+	@ Override
+	public String getName()
+	{
+		return "clmin";
+	}
+
+	@ Override
+	public IntPredicate getPredicate( final int[] dataA, final int[] dataB, final IntBuffer dataC )
+	{
+		return i -> Math.min( dataA[ i ], dataB[ i ] ) == dataC.get( i );
+	}
+}
---
+++ test/de/androbin/opencl/CLTest.java
@@ -0,0 +1,11 @@
+package de.androbin.opencl;
+
+import java.nio.*;
+import java.util.function.*;
+
+public interface CLTest
+{
+	String getName();
+	
+	IntPredicate getPredicate( final int[] dataA, final int[] dataB, final IntBuffer dataC );
+}
